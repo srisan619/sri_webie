4319308468f0f32f17c62a671074a6e63088995c
Srinivasan Irulappan <srisan619@gmail.com>
2026-01-21 19:15:53 +0100

    Completed users app, Roles table, Custom User model, Role-based access logic, Admin management

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Role(models.Model):
+    role_name =models.CharField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.role_name
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class User(AbstractUser):
+    role = models.ForeignKey(Role, on_delete=models.PROTECT, null=True, blank=True)
+    groups = models.ManyToManyField('auth.Group', related_name='custom_user_set', blank=True, help_text='The groups this user belongs to', verbose_name='groups')
+    user_permissions = models.ManyToManyField('auth.Permission', related_name='custom_user_set', blank=True, help_text='Specific permissions for this user.', verbose_name='user permissions')
--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def dashboard(request):
+    if request.user.is_authenticated and request.user.role:
+        if request.user.role.role_name == "admin":
+            return HttpResponse("Welcome Admin")
+        elif request.user.role.role_name == "auditor":
+            return HttpResponse("Welcome Auditor(Read only)")
+    return HttpResponse("Welcome user")
