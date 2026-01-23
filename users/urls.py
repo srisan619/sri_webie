b795db09d525630be1c84d6d9cfa4a25f75f1968
Srinivasan Irulappan <srisan619@gmail.com>
2026-01-23 12:29:20 +0100

    LOGIN & LOGOUT is implemented

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import dashboard
+from .views import dashboard, user_login, user_logout
 
 urlpatterns=[
-    path('dashboard/', dashboard)
+    path('dashboard/', dashboard, name="dashboard"),
+    path('login/', user_login, name="login"),
+    path('logout/', user_logout, name="logout"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 def dashboard(request):
     if request.user.is_authenticated and request.user.role:
@@ -8,3 +10,26 @@ def dashboard(request):
         elif request.user.role.role_name == "auditor":
             return HttpResponse("Welcome Auditor(Read only)")
     return HttpResponse("Welcome user")
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect("dashboard")
+        else:
+            return render(request, "users/login.html", {"error": "Invalid credentials"})
+        
+    return render(request, "users/login.html")
+
+def user_logout(request):
+    logout(request)
+    return redirect("login")
+
+@login_required
+def dashboard(request):
+    return render(request, "users/dashboard.html")
